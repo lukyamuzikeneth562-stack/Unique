18b105a712acb2fe44587e7a381a85e8c17757a4
lukyamuzikeneth562-stack <lukyamuzikeneth562@gmail.com>
2026-02-05 12:08:45 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'textbook-permanent-v1';
+const CACHE_NAME = 'textbook-vfinal-cache';
 const ASSETS = [
   './',
   './index.html',
@@ -8,26 +8,24 @@ const ASSETS = [
   'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.worker.min.js'
 ];
 
-// 1. Install Event - This saves the files to the phone
+// Force immediate caching
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('Caching book for offline use...');
       return cache.addAll(ASSETS);
-    }).then(() => self.skipWaiting()) // Forces the new worker to take over
+    }).then(() => self.skipWaiting())
   );
 });
 
-// 2. Activate Event - Cleans up old versions
+// Take control of the app immediately
 self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim());
 });
 
-// 3. Fetch Event - This is the "Magic" that works without internet
+// Serve files from cache first (Offline mode)
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      // Return the cached file if found, otherwise try the network
       return response || fetch(event.request);
     })
   );
