0966b6b6a65866d85bdb8e9ff2a5d008836c72c0
lukyamuzikeneth562-stack <lukyamuzikeneth562@gmail.com>
2026-02-06 16:33:31 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
-const CACHE_NAME = 'maths-v12-topics';
-const ASSETS = [
+const CACHE_NAME = 'maths-v2';
+const assets = [
   './',
   './index.html',
-  './manifest.json',
   './topic1.pdf', './topic2.pdf', './topic3.pdf', './topic4.pdf',
   './topic5.pdf', './topic6.pdf', './topic7.pdf', './topic8.pdf',
   './topic9.pdf', './topic10.pdf', './topic11.pdf', './topic12.pdf',
@@ -10,12 +9,10 @@ const ASSETS = [
   'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.worker.min.js'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
