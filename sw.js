44b7e8e4636df692e518fe3e06ddc84b0f7204d7
lukyamuzikeneth562-stack <lukyamuzikeneth562@gmail.com>
2026-02-09 15:46:40 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,25 @@
-const CACHE_NAME = 'maths-v7';
+const CACHE_NAME = 'maths-v4';
 const assets = [
   './',
   './index.html',
+  './topic1.pdf', './topic2.pdf', './topic3.pdf', './topic4.pdf',
+  './topic5.pdf', './topic6.pdf', './topic7.pdf', './topic8.pdf',
+  './topic9.pdf', './topic10.pdf', './topic11.pdf', './topic12.pdf',
   'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.min.js',
   'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.worker.min.js'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Caching all work for offline use...');
+      return cache.addAll(assets);
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(cachedResponse => {
-      // IF THE FILE IS IN CACHE (Phone Memory), RETURN IT INSTANTLY (0 Data Used)
-      if (cachedResponse) {
-        return cachedResponse;
-      }
-
-      // IF NOT IN CACHE, DOWNLOAD IT AND SAVE IT FOR NEXT TIME
-      return fetch(event.request).then(networkResponse => {
-        return caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
-        });
-      });
-    })
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
